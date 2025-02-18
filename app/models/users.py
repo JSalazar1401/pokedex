4e5925e0893858f05d6f95cba339bba1debfd05e
JSalazar1401 <jsalazar@utma.edu.mx>
2025-02-17 18:56:34 -0600

    week 6 - factory y creacion de schemas

--- app/models/users.py
+++ app/models/users.py
@@ -1,35 +1,11 @@
 from app import mongo
+from app.models.super_clase import SuperClass
 
-class Users:
-    collection = mongo.db.users
-
-    @staticmethod
-    def find_all():
-        users = Users.collection.find()
-        return list(users)
-
-    @staticmethod
-    def find_by_id(user_id):
-        user = Users.collection.find_one({
-            "_id": user_id
-        })
-        return user
+class User(SuperClass):
+    def __init__(self):
+        super().__init__("users")
     
-    @staticmethod
-    def create(data):
-        user = Users.collection.insert_one(data)
-        return user.inserted_id
-
-    @staticmethod
-    def update(user_id, data):
-        user = Users.collection.update_one({
-            "_id":user_id
-        },{
-            "$set":data
-        })
-        return user
+    def find_all(self):
+        raise NotImplementedError("No es necesario obtener todos los usuarios")
     
-    @staticmethod
-    def delete(user_id):
-        return Users.collection.delete_one({"_id":user_id})
         
